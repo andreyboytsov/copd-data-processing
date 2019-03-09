0479b3480ece230d23b514526ba95ef181825607
andreyboytsov <andrey.m.boytsov@gmail.com>
2019-03-09 17:16:26 +0100

    Generation of weather/patients file by min time

--- Basel/OnlyDate_OnlyTime.py
+++ Basel/OnlyDate_OnlyTime.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+import os
+
+fname = "Patients_and_Weather.csv"
 
 # Read the csv files
 weather_df = pd.read_csv("weather_data.csv")
@@ -25,17 +29,31 @@ patients_df['patients_only_time'] = pd.to_datetime(patients_df['patients_only_ti
 ### Merge the file to create a csv with the headers combined
 patients_and_weather_df = patients_df.merge(weather_df, how='left', left_on=['patients_only_date'], right_on=['weather_only_date'])
 
+patients_and_weather_df["time_difference"] = patients_and_weather_df.apply(lambda x: np.abs(
+                                               (x['patients_only_time'].hour - x['weather_only_time'].hour)*3600+
+                                               (x['patients_only_time'].minute - x['weather_only_time'].minute)*60+
+                                               (x['patients_only_time'].second - x['weather_only_time'].second)),
+                                               axis=1)
+
+pawdf_grouped = patients_and_weather_df.groupby(by=["Merida ID", "Assess_Date_With_Time"])
+
+def select_lowest_time_diff(x):
+    index = np.where(x["time_difference"] == x["time_difference"].min())[0][0]
+    return x.iloc[index:index+1, :]
+
+patients_and_weather_df = pawdf_grouped.apply(select_lowest_time_diff)
+
 ### delete all rows in the dataframe and keep only the header so we can append later
 #patients_and_weather_final = patients_and_weather_df.iloc[0:0]
-patients_and_weather_df.to_csv("Patients_and_Weather.csv")
+patients_and_weather_df.to_csv(fname)
 
-for index, row in patients_and_weather_df.iterrows():
+#for index, row in patients_and_weather_df.iterrows():
     #print(index, row['Region'])
-    if (row['patients_only_date'] == row['weather_only_date']) and (row['patients_only_time'] > row['weather_only_time']):
-        continue
+#    if (row['patients_only_date'] == row['weather_only_date']) and (row['patients_only_time'] > row['weather_only_time']):
+#        continue
 
         #patients_and_weather_df.drop([index],axis = 0, inplace = True)
-    print(index)
+#    print(index)
 #patients_and_weather_df = patients_and_weather_df.groupby('patients_only_date')
 
 
